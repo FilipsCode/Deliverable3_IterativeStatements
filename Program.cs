79e58a9748fdd2b613be34184a7d2d15f1bf50bc
Filip Petersson <122743947+FilipsCode@users.noreply.github.com>
2023-01-31 16:09:16 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Deliverable_3IterativeStatements;
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Console.Write("Enter an integer number between 1 and 100");
+        int input = int.Parse(Console.ReadLine());
+
+        Console.Write("Specify the series type: Even or Odd");
+        string series = Console.ReadLine();
+
+        for (int i = -1; i < input; i = i + 2) ;
+        {
+            Console.WriteLine(i);
+        }
+        
+    }
+}
+
